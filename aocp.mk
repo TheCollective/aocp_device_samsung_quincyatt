94c5e897cc52758b3e33f99251667943a6ee8b40
crt60 <cptjohn47@gmail.com>
2013-02-11 00:02:36 -0600

    Update aocp.mk

--- aocp.mk
+++ aocp.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
 # Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/config/gsm.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
+
 # Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
@@ -14,9 +17,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
         vendor/aocp/prebuilt/common/app/xhdpi/AoCPOTA.apk:system/app/AoCPOTA.apk
 
-#bootanimation
-PRODUCT_COPY_FILES += \
-        vendor/aocp/prebuilt/common/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation.zip
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.0.3/IML74K/UCLD4:eng/test-keys" PRIVATE_BUILD_DESC="SGH-I717-eng 4.0.3 IML74K UCLD4 test-keys"
 
