18840538431acffac8df12e7f6a06d5506882f74
Roger Michaud <cptjohn47@gmail.com>
2013-02-13 00:28:03 -0600

    Move product copy files

--- aocp.mk
+++ aocp.mk
@@ -9,16 +9,7 @@ $(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
 # Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
-# S-Pen App
-PRODUCT_COPY_FILES += \
-        vendor/aocp/prebuilt/common/app/Memo-0-1.5.apk:system/app/Memo-0-1.5.apk
-
-#OTA
-PRODUCT_COPY_FILES += \
-        vendor/aocp/prebuilt/common/app/xhdpi/AoCPOTA.apk:system/app/AoCPOTA.apk
-
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.0.3/IML74K/UCLD4:eng/test-keys" PRIVATE_BUILD_DESC="SGH-I717-eng 4.0.3 IML74K UCLD4 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.0.4/IMM76D/UCLE3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.0.4 IMM76D UCLE3 release-keys"
 
 TARGET_BOOTANIMATION_NAME := bootanimation_1280_800
 
