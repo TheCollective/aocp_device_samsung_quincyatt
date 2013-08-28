0ef6ca42f527f2bb6a91f50e9c3bd32bfe9d996a
fezzy <fezzy@fezzy-VirtualBox.(none)>
2013-08-28 15:57:00 -0400

    'Update '

--- aocp.mk
+++ aocp.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/aocp/config/nfc_enhanced.mk)
 # Inherit some common aocp stuff.
 $(call inherit-product, vendor/aocp/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.0.4/IMM76D/UCLE3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.0.4 IMM76D UCLE3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.2/JZO54K/I717UCMD3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.2 JZO54K I717UCMD3 release-keys"
 
 TARGET_BOOTANIMATION_NAME := bootanimation_1280_800
 
